c33b1b536ac7adc53f9c4a5d7b4125264cddcc07
Junne <junne.bai@gmail.com>
2017-02-14 16:03:59 +0800

    add Home Page

---
+++ PRHomeModule/PRHomeModule/PRHomeViewController.m
@@ -0,0 +1,37 @@
+//
+//  PRHomeViewController.m
+//  PRHomeModule
+//
+//  Created by baijf on 14/02/2017.
+//  Copyright © 2017 Junne. All rights reserved.
+//
+
+#import "PRHomeViewController.h"
+
+@interface PRHomeViewController ()
+
+@end
+
+@implementation PRHomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
